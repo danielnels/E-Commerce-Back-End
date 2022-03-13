246f15b474affee18d3c9d7a0f13699c6a727c90
danielnels <danieln@newground.net.au>
2022-03-13 11:42:04 +0800

    added video demo files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.post('/', async(req, res) => {
 
   try {
       const categoryData = await Category.create(req.body);
-      res.status(201).json(categoryData);
+      res.status(200).json(categoryData);
   } catch (err) {
       res.status(500).json(err);
   }
